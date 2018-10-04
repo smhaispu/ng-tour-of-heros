8414a63b4501bef69f419d417663e5f1da1dc386
Mhaispurkar <smhaispurkar@kpmg.com>
2018-10-04 11:43:57 +0530

    Tour of Heros

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService }  from '../hero.service';
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent{
+@Input() hero:Hero
+constructor(
+  private route: ActivatedRoute,
+  private heroService: HeroService,
+  private location: Location
+) {}
+ngOnInit(): void {
+  this.getHero();
+}
+save(): void {
+  this.heroService.updateHero(this.hero)
+    .subscribe(() => this.goBack());
+}
+goBack(): void {
+  this.location.back();
+}
+
+getHero(): void {
+  const id = +this.route.snapshot.paramMap.get('id');
+  this.heroService.getHero(id)
+    .subscribe(hero => this.hero = hero);
+}
+}
